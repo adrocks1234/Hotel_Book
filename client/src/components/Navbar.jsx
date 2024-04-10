8eee5fa7ab5f036b5c8f0ff2b6ca1d30d96d26b3
Arijit Dey <71665909+adrocks1234@users.noreply.github.com>
2024-04-10 17:19:06 +0530

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -60,7 +60,7 @@ const Navbar = () => {
             <Person sx={{ color: variables.darkgrey }} />
           ) : (
             <img
-              src={`http://localhost:3000/${user.profileImagePath.replace(
+              src={`https://hotel-book-ljxt.onrender.com/${user.profileImagePath.replace(
                 "public",
                 ""
               )}`}
