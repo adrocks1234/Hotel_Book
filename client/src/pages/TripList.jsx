9d2b1641bfa8777bab825d6eea99cbf71112240e
Arijit Dey <71665909+adrocks1234@users.noreply.github.com>
2024-04-10 17:28:26 +0530

    Update TripList.jsx

--- client/src/pages/TripList.jsx
+++ client/src/pages/TripList.jsx
@@ -17,7 +17,7 @@ const TripList = () => {
   const getTripList = async () => {
     try {
       const response = await fetch(
-        `http://localhost:3000/users/${userId}/trips`,
+        `https://hotel-book-ljxt.onrender.com/users/${userId}/trips`,
         {
           method: "GET",
         }
